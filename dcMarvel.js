94c7abb155852d570f232bf5e3fb0d2ecaa8e4d0
Matthew Sucharski <a7x1013@aol.com>
2018-02-12 19:06:41 -0500

    only css problems

--- dcMarvel.js
+++ dcMarvel.js
@@ -1,10 +1,8 @@
 console.log('things linked correctly')
 let index = 0;
 //const answers = {}
-var score = 0;
-
+let score = 0;
 //below are the questions in an object
-
 const questions = [
   {
     question: " How many children does Batman have? (not adopted)",
@@ -12,7 +10,6 @@ const questions = [
     b: "Two",
     c: "Zero",
     correctAnswer: 'Two',
-
 },
 {
     question: " How does Cyclopses beam blast work?",
@@ -20,7 +17,6 @@ const questions = [
     b: "He has the ability to change the kinetic energy of the photons in his sight to redirect them to what he is looking at",
     c: "His eyes are a portal to another dimension and its energy coming from that dimension.",
     correctAnswer: 'His eyes are a portal to another dimension and its energy coming from that dimension.',
-
 },
 {
     question: " True or False, Wonder Woman's creator was into bondage which is why she has a rope that makes men tell the truth and wears shackles on her wrists.",
@@ -28,7 +24,6 @@ const questions = [
     b: "False",
     c: "I've noticed but never put much thought into it.",
     correctAnswer: 'True',
-
 },
 {
     question: " In Watchmen (1986) how was Ozymandias stopped?",
@@ -36,7 +31,6 @@ const questions = [
     b: "He comes to his sences that killing thousands to save millions is not the way to do things",
     c: "He isnt stopped because he set his plans into motion 35 minutes ago",
     correctAnswer: 'He isnt stopped because he set his plans into motion 35 minutes ago',
-
 },
 {
     question: " What two things does the 'S' on Supermans chest stand for?",
@@ -44,7 +38,6 @@ const questions = [
     b: "Brave, Super",
     c: "It is an insignia of the house of El, Hope",
     correctAnswer: 'It is an insignia of the house of El, Hope',
-
 },
 {
     question: " Who is an omega level mutant?",
@@ -52,7 +45,6 @@ const questions = [
     b: "Colossus",
     c: "Iceman",
     correctAnswer: 'Iceman',
-
 },
 {
     question: " Which age Superman is strongest?",
@@ -60,7 +52,6 @@ const questions = [
     b: "Silver",
     c: "Modern",
     correctAnswer: 'Silver',
-
 },
 {
     question: " What is the name of Beta Ray Bill's hammer?",
@@ -68,7 +59,6 @@ const questions = [
     b: "Leetha of the 7th House of K",
     c: "Gungnir",
     correctAnswer: 'Stormbreaker',
-
 },
 {
     question: " In order who used the name The Flash?",
@@ -76,7 +66,6 @@ const questions = [
     b: "Jason Todd, Wally West, Terry Mcginnis",
     c: "Jay Garrick, Barry Allen, Wally West",
     correctAnswer: 'Jay Garrick, Barry Allen, Wally West',
-
 },
 {
     question: " Why is Lobo the last Czarnian?",
@@ -84,12 +73,9 @@ const questions = [
     b: "They are an ancient race long gone",
     c: "Lobo killed them all so he can be unique",
     correctAnswer: 'Lobo killed them all so he can be unique',
-
   },
 ];
-
-
-function renderQuestion() {
+const renderQuestion = () => {
   let currentQuestion = questions[index];
   let q = document.querySelector('#question');
   let a = document.querySelector('#one');
@@ -101,34 +87,29 @@ function renderQuestion() {
   c.innerHTML = currentQuestion.c;
 }
 renderQuestion();
-
-function nextQuestion() {
+const nextQuestion = () => {
   index++;
   renderQuestion();
-  }
-
+}
 //reset quiz
-function resetQuiz() {
+const resetQuiz = () => {
   score = 0;
   index = 0;
   console.log(score);
   renderQuestion();
 }
-
 let reset = document.querySelector('#reset');
-reset.addEventListener('click', function(event) {
-  resetQuiz();
-})
+reset.addEventListener('click', event => resetQuiz())
 
 let answer = document.querySelector('.answers');
-
-    answer.addEventListener('click', function(event) {
+    answer.addEventListener('click', event => {
       if (event.target.innerHTML == questions[index].correctAnswer) {
-        score += 1;
+        let newScore = score += 1
+        let total = document.querySelector(".score")
+        total.innerHTML = `Score: ${newScore}`
       }
-      console.log(score);
-   nextQuestion();
+   console.log(score)
+   nextQuestion()
  })
-
- let total = document.querySelector('.score');
-    total.innerHTML = "Score: " + score;
+ let total = document.querySelector('.score')
+    total.innerHTML = `Score: ${score}`
